4fb1d939567f950b4bbd0a39e02163ea96d8fd86
Percy Mamedy <percymamedy@gmail.com>
2016-01-31 11:57:55 +0400

    Initialise

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# laravel-watson-translate
-Using IBM Watson translation API with laravel 5
+# Laravel 5 IBM Watson Translate
+
+This package provides an api to perform translations using the IBM Watson Language Translation service. 
+
+Read the docs for Watson Language Translation service [here](https://www.ibm.com/smarterplanet/us/en/ibmwatson/developercloud/doc/language-translation/) first.
---
+++ src/WatsonTranslateServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace FindBrok\WatsonTranslate;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class WatsonTranslateServiceProvider
+ * @package FindBrok\WatsonTranslate
+ */
+class WatsonTranslateServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
